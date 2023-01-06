2ce67284e537df8ad50213cb19bc59030eecf06d
adil <brokenaedil@gmail.com>
2023-01-06 19:11:10 +0530

    Disco dancer

---
+++ simple.java
@@ -0,0 +1,8 @@
+// 1st.....SIMPLE PROGRAM TO PRINT...  
+public class simple
+{                                                 
+public static void main(String[] args) 
+    {
+      System.out.println("Hello , I am a disco dancer..🕺🕺");
+    }
+}
