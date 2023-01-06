b491d25a4b391ebec6563b9503ef5b429e1eab4c
adil <brokenaedil@gmail.com>
2023-01-06 19:44:39 +0530

    Command line argument

---
+++ argument.java
@@ -0,0 +1,10 @@
+//3rd....FOR COMMAND LINE ARGUMENT IN JAVA..
+public class argument 
+{
+    public static void main(String[] args) {
+        for(String a: args)
+        {
+            System.out.println(a);
+        }
+    }
+}
