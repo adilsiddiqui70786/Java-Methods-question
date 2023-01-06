3422d93271cedab849eb3ac7cd51f08bab49466c
adil <brokenaedil@gmail.com>
2023-01-06 19:39:59 +0530

    looping of integer

---
+++ integer.java
@@ -0,0 +1,14 @@
+//2nd... FOR PRINTING THE INETEGERS..
+public class integer
+{
+    public static void main(String[] args)
+    {
+     int p; //For declaring the variable..
+    
+     /* USING OF FOR LOOP TO REPEAT INSTRUCTION EXECUTION.. */
+     for (p = 1; p<=15; p++)
+     {
+        System.out.println(p);
+     }
+    }    
+}
