69abdb8bd1f2bed783de5e62c78f48ec66ed872d
adil <brokenaedil@gmail.com>
2023-01-10 21:41:59 +0530

    Normal using of if else Clause

---
+++ normalifelse.java
@@ -0,0 +1,14 @@
+// 10th ... normal using of if and else..
+public class normalifelse {
+    public static void main(String[] args) {
+        boolean learning = true;
+        if (learning)
+        {
+            System.out.println("Android Studio");
+        }
+        else
+        {
+            System.out.println("Are you a coder ?");
+        }
+    }
+}
